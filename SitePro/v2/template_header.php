d66dd9c541dcdb36fb2372d001287db6effab3df
Rbd-Antoine <122039637+Rbd-Antoine@users.noreply.github.com>
2023-03-14 09:44:04 +0100

    Début TP PHP

---
+++ SitePro/v2/template_header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css" type="text/css"
+            media="screen" title="default" charset="utf-8" />
+            <html lang="en" >
+            <meta charset="UTF-8">
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Rubik&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="./style.css">
+            <script src="https://kit.fontawesome.com/117e73e7ed.js" crossorigin="anonymous"></script>
+            <script  src="./script.js"></script>
+    </head>
+    
