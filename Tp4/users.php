bffe6c13d128db9a603b26a04da1ede686b38288
Rbd-Antoine <122039637+Rbd-Antoine@users.noreply.github.com>
2023-03-20 12:03:21 +0100

    Exo 4 Tp 4

--- Tp4/users.php
+++ Tp4/users.php
@@ -14,12 +14,15 @@
     }
 
     //delete all tables from database dbtest
-    $request = $pdo->prepare("drop table if exists users");
+    /*$request = $pdo->prepare("drop table if exists users");
     $request->execute();
-
+    */
+    /*
+    //create table users
     $sql = file_get_contents('users.sql');
     $request = $pdo->exec($sql);
 
+    */
     // retrieve data from database using fetch(PDO::FETCH_OBJ)
     $request = $pdo->prepare("select * from users");
     $request->execute();
@@ -36,6 +39,54 @@
         echo '</tr>';
     }
     echo '</table>';
+
+    //form to add another user
+    echo '<form action="users.php" method="post">';
+    echo '<input type="text" name="nom" placeholder="nom">';
+    echo '<input type="text" name="email" placeholder="email">';
+    echo '<input type="submit" value="Ajouter">';
+    echo '</form>';
+
+    //add a new user
+    if(isset($_POST['nom']) && isset($_POST['email'])) {
+        $request = $pdo->prepare("insert into users(nom,email) values(:nom,:email)");
+        $request->bindParam(':nom',$_POST['nom']);
+        $request->bindParam(':email',$_POST['email']);
+        $request->execute();
+        header('Location: users.php');
+    }
+
+    //button to delete one user
+    echo '<form action="users.php" method="post">';
+    echo '<input type="text" name="id" placeholder="id">';
+    echo '<input type="submit" value="Supprimer">';
+    echo '</form>';
+
+    //delete one user
+    if(isset($_POST['id'])) {
+        $request = $pdo->prepare("delete from users where id=:id");
+        $request->bindParam(':id',$_POST['id']);
+        $request->execute();
+        header('Location: users.php');
+    }
+
+    //button to update one user
+    echo '<form action="users.php" method="post">';
+    echo '<input type="text" name="id" placeholder="id">';
+    echo '<input type="text" name="nom" placeholder="nom">';
+    echo '<input type="text" name="email" placeholder="email">';
+    echo '<input type="submit" value="Modifier">';
+    echo '</form>';
+
+    //update one user
+    if(isset($_POST['id']) && isset($_POST['nom']) && isset($_POST['email'])) {
+        $request = $pdo->prepare("update users set nom=:nom,email=:email where id=:id");
+        $request->bindParam(':id',$_POST['id']);
+        $request->bindParam(':nom',$_POST['nom']);
+        $request->bindParam(':email',$_POST['email']);
+        $request->execute();
+        header('Location: users.php');
+    }
 
     // close connection
     $pdo = null;
